dd80666da8ccafee4dc53c974d5c592b8a4ceeba
ravi maithrey <maitrey.ind@gmail.com>
2021-08-10 15:28:05 +0530

    searching pattern in file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,31 +1,40 @@
 // using regex and clap to build our command line tool
-use regex::Regex;
+// using file and bufReader to parse file input
 use clap::{App, Arg};
+use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
-// defining the clap App
+    // defining the clap App
     let args = App::new("grep-lite")
-    .version("0.3")
-    .about("search for patterns in text")
-    .arg(Arg::with_name("pattern")
-    .help("The pattern to search for")
-    .takes_value(true)
-    .required(true))
-    .get_matches();
+        .version("0.4")
+        .about("search for patterns in text")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search the pattern in")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
-    let quote = "\
-    Every face, every shop,
-    bedroom window, public-house, and 
-    dark square is a picture 
-    feverishly turned--in search of what?
-    It is the same with books.
-    What do we seek through 
-    millions of pages?";
 
-    for line in quote.lines(){
-        match re.find(line){
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
